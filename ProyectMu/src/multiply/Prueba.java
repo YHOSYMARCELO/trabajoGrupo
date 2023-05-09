ea3a0da9ce7b5d378f238c7b951840bd11e406db
YHOSY <jsmarceloc@ceinmark.net>
2023-05-09 19:51:36 +0200

    commit

---
+++ ProyectMu/src/multiply/Prueba.java
@@ -0,0 +1,16 @@
+package multiply;
+
+public class Prueba {
+	
+		public static void main(String args[]) {
+			multiply();
+	}
+		public static void multiply() {
+			for (int i=1; i<=10; i++) {
+				for (int j=0; j<=12; j++) {
+					System.out.println(i + " * "+ j + " = "+ i*j);
+				}
+			}
+		}
+	
+}
