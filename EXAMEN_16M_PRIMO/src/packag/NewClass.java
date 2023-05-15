38a278db0b67c8a1d5927d7068398627280aa590
YHOSY <jsmarceloc@ceinmark.net>
2023-05-15 09:17:52 +0200

    nuevo commit

--- EXAMEN_16M_PRIMO/src/packag/NewClass.java
+++ EXAMEN_16M_PRIMO/src/packag/NewClass.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class NewClass {
 	public static void main(String args[]) {
-		System.out.println("Hola");
+		System.out.println("Buenos días Ceinmak, Soy Joselyn Marcelo");
 		Scanner sc= new Scanner(System.in); 
-		System.out.println("Introduce un numero");
+		System.out.println("Introduce un numero para averiguar si es primo o no");
 		int num=sc.nextInt(); 
 		System.out.println(num);
 	}
